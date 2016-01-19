5dfee2f98aac3ba2ece3f018f700eb84fdf86812
Stacey Mulcahy <stacey.mulcahy@gmail.com>
2016-01-18 23:14:36 -0800

    data for list

--- code/RoomReminder/RoomReminder/js/list.js
+++ code/RoomReminder/RoomReminder/js/list.js
@@ -1,1 +1,18 @@
 ﻿
+
+(function () {
+    "use strict";
+    var items = [{ "number": "2340", "from": "03/23/2015" }, { "number": "4456", "from": "03/21/2015" }, { "number": "0098", "from": "03/20/2015" }];
+    WinJS.Namespace.define("Rooms.ListView", {
+        data: new WinJS.Binding.List(items)
+    })
+
+    var page = WinJS.UI.Pages.define("html/list.html", {
+        ready: function (element, options) {
+           
+
+        }
+    })
+
+   
+})();
