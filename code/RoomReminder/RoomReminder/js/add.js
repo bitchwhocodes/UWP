1ae6c4703f1b0b72c71b600ad03a58ff3f009a3f
Stacey Mulcahy <stacey.mulcahy@gmail.com>
2016-01-18 21:59:28 -0800

    defined page for add.html

--- code/RoomReminder/RoomReminder/js/add.js
+++ code/RoomReminder/RoomReminder/js/add.js
@@ -1,1 +1,8 @@
-﻿
+﻿(function () {
+    "use strict";
+    var page = WinJS.UI.Pages.define("html/add.html", {
+        ready: function (element, options) {
+            console.log("Add page ready");
+        }
+    })
+})();
