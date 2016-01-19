85b1c1c34dd237536e4f0c5009e5949e0b6c3703
Stacey Mulcahy <stacey.mulcahy@gmail.com>
2016-01-18 21:17:14 -0800

    Coded the Page object

--- code/RoomReminder/RoomReminder/js/home.js
+++ code/RoomReminder/RoomReminder/js/home.js
@@ -1,1 +1,17 @@
-﻿
+﻿(function () {
+    "use strict";
+    
+    var page = WinJS.UI.Pages.define("/html/home.html", {
+        ready: function (element, options) {
+            addbutton.addEventListener("click", transitionBetweenPages, false);
+            allbutton.addEventListener("click", transitionBetweenPages, false);
+        }
+    })
+
+    function transitionBetweenPages(obj)
+    {
+        var id = obj.currentTarget.id;
+        console.log("button selected".id);
+    }
+
+})();
